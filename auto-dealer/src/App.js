27fc260ea14be761f993d77141e55b4f284474d1
uimarshall <uimarshall@gmail.com>
2020-04-12 16:20:07 +0100

    refactor App.js

--- auto-dealer/src/App.js
+++ auto-dealer/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Header from "./components/layout/Header";
+import Skills from "./components/pages/Skills";
+import Preloader from "./components/utils/Preloader";
+import Stock from "./components/pages/Stock";
+import SearchDiv from "./components/utils/SearchDiv";
+import FeaturedCar from "./components/utils/FeaturedCar";
+import Footer from "./components/layout/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Preloader />
+				<Navbar />
+				<Header />
+				<Skills />
+				<Stock />
+				<SearchDiv />
+				<FeaturedCar />
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
